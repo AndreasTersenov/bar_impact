e32f8490c019945ee13f67dfa1baa69b7529f303
Andreas Tersenov <atersenov@physics.uoc.gr>
2026-01-25 16:37:53 +0000

    added init in processing

--- src/bar_impact/processing/__init__.py
+++ src/bar_impact/processing/__init__.py
@@ -5,14 +5,22 @@ This module contains functions for processing HEALPix convergence maps,
 including L1 norm calculations, power spectrum analysis, and peak counting.
 """
 
-from bar_impact.processing.l1_norms import process_l1_norms
-from bar_impact.processing.power_spectrum import process_power_spectrum
-from bar_impact.processing.peak_counts import process_peak_counts
-from bar_impact.processing.bnt_transforms import apply_bnt_transform
+from bar_impact.processing.bnt_transforms import (
+    apply_bnt_transform,
+    get_bnt_matrix,
+    BNT_MATRIX_DEFAULT,
+)
+
+# These will be implemented in Phase 2
+# from bar_impact.processing.l1_norms import process_l1_norms
+# from bar_impact.processing.power_spectrum import process_power_spectrum
+# from bar_impact.processing.peak_counts import process_peak_counts
 
 __all__ = [
-    "process_l1_norms",
-    "process_power_spectrum",
-    "process_peak_counts",
     "apply_bnt_transform",
+    "get_bnt_matrix",
+    "BNT_MATRIX_DEFAULT",
+    # "process_l1_norms",
+    # "process_power_spectrum",
+    # "process_peak_counts",
 ]
