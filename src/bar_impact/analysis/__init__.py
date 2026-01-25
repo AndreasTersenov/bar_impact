83631f9ad64fdcde548e34c9f3c8e18e757077ea
Andreas Tersenov <atersenov@physics.uoc.gr>
2026-01-25 17:56:58 +0000

    update analysis init

--- src/bar_impact/analysis/__init__.py
+++ src/bar_impact/analysis/__init__.py
@@ -5,10 +5,38 @@ This module provides utilities for aggregating processed data,
 performing statistical analysis, and creating visualizations.
 """
 
-from bar_impact.analysis.aggregation import aggregate_results
-from bar_impact.analysis.visualization import visualize_coverage
+from bar_impact.analysis.aggregation import (
+    ResultsAggregator,
+    AggregationConfig,
+    aggregate_results,
+    aggregate_l1_norms,
+    aggregate_power_spectra,
+    load_datavectors,
+)
+from bar_impact.analysis.visualization import (
+    PosteriorPlotter,
+    CoveragePlotter,
+    PowerSpectrumPlotter,
+    PlotConfig,
+    visualize_coverage,
+    plot_power_spectrum,
+    plot_triangle,
+)
 
 __all__ = [
+    # Aggregation
+    "ResultsAggregator",
+    "AggregationConfig",
     "aggregate_results",
+    "aggregate_l1_norms",
+    "aggregate_power_spectra",
+    "load_datavectors",
+    # Visualization
+    "PosteriorPlotter",
+    "CoveragePlotter",
+    "PowerSpectrumPlotter",
+    "PlotConfig",
     "visualize_coverage",
+    "plot_power_spectrum",
+    "plot_triangle",
 ]
